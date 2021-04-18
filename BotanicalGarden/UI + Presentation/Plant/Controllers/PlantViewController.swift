2d248ca248aa0d115172d4dfc12597df97534ed9
Terry <terry86062@gmail.com>
2021-04-18 14:24:31 +0800

    feat: create PlantViewController .swift and .xib

---
+++ BotanicalGarden/UI + Presentation/Plant/Controllers/PlantViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PlantViewController.swift
+//  BotanicalGarden
+//
+//  Created by 黃偉勛 Terry on 2021/4/18.
+//
+
+import UIKit
+
+class PlantViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
