c22b26dc2b911b91e8181f8c9342644502c67665
Terry <terry86062@gmail.com>
2021-04-18 20:11:44 +0800

    feat: create MainQueueDispatchDecorator

---
+++ BotanicalGarden/UI + Presentation/Thread/MainQueueDispatchDecorator.swift
@@ -0,0 +1,23 @@
+//
+//  MainQueueDispatchDecorator.swift
+//  BotanicalGarden
+//
+//  Created by 黃偉勛 Terry on 2021/4/18.
+//
+
+import Foundation
+
+final class MainQueueDispatchDecorator<T> {
+    private let decoratee: T
+
+    init(decoratee: T) {
+        self.decoratee = decoratee
+    }
+
+    func dispatch(completion: @escaping () -> Void) {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async(execute: completion)
+        }
+        completion()
+    }
+}
