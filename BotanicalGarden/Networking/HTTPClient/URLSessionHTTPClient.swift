a463eae75163b0de804ae6b38ef72043dd80c97e
Terry <terry86062@gmail.com>
2021-04-18 11:43:15 +0800

    feat: create URLSessionHTTPClient class which conforms HTTPClient protocol

---
+++ BotanicalGarden/Networking/HTTPClient/URLSessionHTTPClient.swift
@@ -0,0 +1,32 @@
+//
+//  URLSessionHTTPClient.swift
+//  BotanicalGarden
+//
+//  Created by 黃偉勛 Terry on 2021/4/18.
+//
+
+import Foundation
+
+final class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+
+    private struct UnexpectedError: Error {}
+
+    func get(from request: URLRequest, completion: @escaping (HTTPClient.Result) -> Void) {
+        session.dataTask(with: request) { data, response, error in
+            completion(Result {
+                if let error = error {
+                    throw error
+                } else if let data = data, let response = response as? HTTPURLResponse {
+                    return (data, response)
+                } else {
+                    throw UnexpectedError()
+                }
+            })
+        }.resume()
+    }
+}
