c1cc1da18eb938cf5a3ac8d80bfb8ed773bde6b3
Terry <terry86062@gmail.com>
2021-04-18 13:54:17 +0800

    feat: create PlantItem struct

---
+++ BotanicalGarden/Feature/Plant/PlantItem.swift
@@ -0,0 +1,15 @@
+//
+//  PlantItem.swift
+//  BotanicalGarden
+//
+//  Created by 黃偉勛 Terry on 2021/4/18.
+//
+
+import Foundation
+
+struct PlantItem {
+    let name: String
+    let location: String
+    let feature: String
+    let imageURL: URL?
+}
