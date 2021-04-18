0b8f4f91f703db4c340c70a7021d4c730107cd95
Terry <terry86062@gmail.com>
2021-04-18 21:28:15 +0800

    feat: extension UIScreen with width static property

---
+++ BotanicalGarden/UI + Presentation/Helpers/UIScreen+BotanicalGarden.swift
@@ -0,0 +1,13 @@
+//
+//  UIScreen+BotanicalGarden.swift
+//  BotanicalGarden
+//
+//  Created by 黃偉勛 Terry on 2021/4/18.
+//
+
+import UIKit
+
+extension UIScreen {
+    
+    static let width = UIScreen.main.bounds.size.width
+}
