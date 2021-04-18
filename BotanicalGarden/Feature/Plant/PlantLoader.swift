8fabd0c38d0a0678077a850f16da0be14fd1b51c
Terry <terry86062@gmail.com>
2021-04-18 13:55:54 +0800

    feat: create PlantLoader protocol

---
+++ BotanicalGarden/Feature/Plant/PlantLoader.swift
@@ -0,0 +1,14 @@
+//
+//  PlantLoader.swift
+//  BotanicalGarden
+//
+//  Created by 黃偉勛 Terry on 2021/4/18.
+//
+
+import Foundation
+
+protocol PlantLoader {
+    typealias Result = Swift.Result<[PlantItem], Error>
+    
+    func load(from request: PlantRequest?, completion: @escaping (Result) -> Void)
+}
