4a607e04a6dd50c3e12f9f834d602a6a1dbcae93
Terry <terry86062@gmail.com>
2021-04-18 16:55:29 +0800

    feat: create PlantCellModel

---
+++ BotanicalGarden/UI + Presentation/Plant/Models/PlantCellModel.swift
@@ -0,0 +1,15 @@
+//
+//  PlantCellModel.swift
+//  BotanicalGarden
+//
+//  Created by 黃偉勛 Terry on 2021/4/18.
+//
+
+import Foundation
+
+struct PlantCellModel {
+    let name: String
+    let location: String
+    let feature: String
+    let imageURL: URL?
+}
