b0bfb87bf13e93ba1760d57a5a01aaf3c43d79c4
Terry <terry86062@gmail.com>
2021-04-18 15:36:40 +0800

    feat: add PlantViewController UI

--- BotanicalGarden/UI + Presentation/Plant/Controllers/PlantViewController.swift
+++ BotanicalGarden/UI + Presentation/Plant/Controllers/PlantViewController.swift
@@ -8,7 +8,14 @@
 import UIKit
 
 class PlantViewController: UIViewController {
+    
+    // MARK: - IBOutlet
+    @IBOutlet weak var navigationTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var navigationLabel: UILabel!
+    @IBOutlet weak var coverNavigationView: UIView!
+    @IBOutlet weak var tableView: UITableView!
 
+    // MARK: - override func
     override func viewDidLoad() {
         super.viewDidLoad()
     }
