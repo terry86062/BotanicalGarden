6ed96aa4711b76d9eb20f29dd2cb1530b04f13d5
Terry <terry86062@gmail.com>
2021-04-18 11:51:24 +0800

    feat: create Request protocol

---
+++ BotanicalGarden/Networking/RemoteLoader/Request/Request.swift
@@ -0,0 +1,14 @@
+//
+//  Request.swift
+//  BotanicalGarden
+//
+//  Created by 黃偉勛 Terry on 2021/4/18.
+//
+
+import Foundation
+
+protocol Request {
+    
+    var url: URL { get }
+    var parameters: [String: Any] { get }
+}
