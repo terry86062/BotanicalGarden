c3f8d45821bb79d7df69de57e1edac161f6dc268
Terry <terry86062@gmail.com>
2021-04-18 12:45:36 +0800

    feat: create RemoteLoader protocol

---
+++ BotanicalGarden/Networking/RemoteLoader/RemoteLoader.swift
@@ -0,0 +1,15 @@
+//
+//  RemoteLoader.swift
+//  BotanicalGarden
+//
+//  Created by 黃偉勛 Terry on 2021/4/18.
+//
+
+import Foundation
+
+protocol RemoteLoader {
+    func load<Req: Request>(
+        from request: Req,
+        completion: @escaping (Result<Req.Mapper.Response, Error>) -> Void
+    )
+}
