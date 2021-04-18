202be15a7962a64bc862a2fc926f93f8a628e7de
Terry <terry86062@gmail.com>
2021-04-18 11:35:20 +0800

    feat: create HTTPClient protocol

---
+++ BotanicalGarden/Networking/HTTPClient/HTTPClient.swift
@@ -0,0 +1,14 @@
+//
+//  HTTPClient.swift
+//  BotanicalGarden
+//
+//  Created by 黃偉勛 Terry on 2021/4/18.
+//
+
+import Foundation
+
+protocol HTTPClient {
+    typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
+
+    func get(from request: URLRequest, completion: @escaping (Result) -> Void)
+}
