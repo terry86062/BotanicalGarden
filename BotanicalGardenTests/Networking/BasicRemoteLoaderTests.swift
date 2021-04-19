ff66003129aca7b44834ee2fcfb86ba96668f442
Terry <terry86062@gmail.com>
2021-04-20 00:39:04 +0800

    test: create BasicRemoteLoaderTests to test BasicRemoteLoader

---
+++ BotanicalGardenTests/Networking/BasicRemoteLoaderTests.swift
@@ -0,0 +1,12 @@
+//
+//  BasicRemoteLoaderTests.swift
+//  BotanicalGardenTests
+//
+//  Created by 黃偉勛 Terry on 2021/4/20.
+//
+
+import XCTest
+
+class BasicRemoteLoaderTests: XCTestCase {
+    
+}
