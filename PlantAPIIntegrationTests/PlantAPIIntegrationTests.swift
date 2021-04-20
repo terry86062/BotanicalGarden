8d05201374d72d3f2ca1f443ef6bfe7d48dd7dd0
Terry <terry86062@gmail.com>
2021-04-20 21:13:26 +0800

    test: create PlantAPIIntegrationTests target to test calling plant API

---
+++ PlantAPIIntegrationTests/PlantAPIIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  PlantAPIIntegrationTests.swift
+//  PlantAPIIntegrationTests
+//
+//  Created by 黃偉勛 Terry on 2021/4/20.
+//
+
+import XCTest
+import BotanicalGarden
+
+class PlantAPIIntegrationTests: XCTestCase {
+
+}
