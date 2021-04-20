633fc69262b289eee870347177ef18887d4516bc
20346 <terry_huang@addcn.com>
2021-04-20 17:25:12 +0800

    test: create PlantViewControllerTests to test PlantViewController

---
+++ BotanicalGardenTests/UI + Presentation/Plant/PlantViewControllerTests.swift
@@ -0,0 +1,13 @@
+//
+//  PlantViewControllerTests.swift
+//  BotanicalGardenTests
+//
+//  Created by 黃偉勛 on 2021/4/20.
+//
+
+import XCTest
+import BotanicalGarden
+
+class PlantViewControllerTests: XCTestCase {
+
+}
