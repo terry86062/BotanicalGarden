a6e292909fafde4c1f40268859cc032b55e6db37
Terry <terry86062@gmail.com>
2021-09-09 11:30:25 +0800

    test: Track memory leaks for PlantViewController

--- BotanicalGardenTests/UI + Presentation/Plant/PlantViewControllerTests.swift
+++ BotanicalGardenTests/UI + Presentation/Plant/PlantViewControllerTests.swift
@@ -11,8 +11,7 @@ import BotanicalGarden
 class PlantViewControllerTests: XCTestCase {
 
     func test_loadPlantActions_requestPlantFromViewModel() {
-        let vm = PlantViewModelSpy()
-        let sut = PlantViewController(viewModel: (vm.inputs, vm.outputs))
+        let (sut, vm) = makeSUT()
 
         XCTAssertEqual(vm.loadPlantCallCount, 0)
 
@@ -21,6 +20,14 @@ class PlantViewControllerTests: XCTestCase {
     }
     
     // MARK: - Helpers
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: PlantViewController, vm: PlantViewModelSpy) {
+        let vm = PlantViewModelSpy()
+        let sut = PlantViewController(viewModel: (vm.inputs, vm.outputs))
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(vm, file: file, line: line)
+        return (sut, vm)
+    }
+    
     private class PlantViewModelSpy: ViewModel, PlantViewModelInputs, PlantViewModelOutputs {
         var inputs: PlantViewModelInputs { return self }
         var outputs: PlantViewModelOutputs { return self }
